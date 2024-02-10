e9f532b5213c2ea6f5117745f4ec7099bcfeab82
shehrozraza <shehroz.raza11@hotmail.com>
2024-02-10 18:12:20 +0500

    Casing

--- index.ts
+++ index.ts
@@ -1,14 +1,3 @@
-let num1=Math.random()*100
-console.log(num1);
+let names="shehroz"
+console.log(names.toUpperCase);
 
-let ceil=Math.floor(num1)
-console.log(ceil);
-
-if(ceil%2==0){
-    console.log(`${ceil} is even`);
-    
-}
-else{
-    console.log(`${ceil} is odd`);
-    
-}
